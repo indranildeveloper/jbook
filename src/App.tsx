7dde2bde5ce3d4805e9e5e1de9be3d3e9b4e8159
Indranil Halder <indranilhalder.dev@gmail.com>
2023-01-29 20:48:39 +0530

    Code running in iframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState, useEffect, MouseEvent } from "react";
+import { useState, useEffect, useRef, MouseEvent } from "react";
 import * as esbuild from "esbuild-wasm";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 
 function App() {
+  const iframe = useRef<any>();
   const [input, setInput] = useState<string>("");
-  const [code, setCode] = useState<string>("");
 
   const startService = async () => {
     await esbuild.initialize({
       worker: true,
-      wasmURL: "/esbuild.wasm",
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.17.5/esbuild.wasm",
     });
   };
 
@@ -20,6 +20,8 @@ function App() {
 
   const handleSubmit = async (event: MouseEvent<HTMLButtonElement>) => {
     // TODO: return if the test input is empty
+
+    iframe.current.srcdoc = html;
 
     const result = await esbuild.build({
       entryPoints: ["index.js"],
@@ -32,9 +34,29 @@ function App() {
       },
     });
 
-    // console.log(result);
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener("message", (event) => {
+            try{
+              eval(event.data);
+            } catch(error) {
+              const root = document.querySelector("#root");
+              root.innerHTML = "<div style='color: red;'><h4>Runtime Error</h4>" + error + "</div>";
+              console.error(error);
+            }
+          }, false)
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -46,7 +68,12 @@ function App() {
         <button onClick={handleSubmit}>Submit</button>
       </div>
 
-      <pre>{code}</pre>
+      <iframe
+        ref={iframe}
+        srcDoc={html}
+        title="code-runner"
+        sandbox="allow-scripts"
+      ></iframe>
     </div>
   );
 }
