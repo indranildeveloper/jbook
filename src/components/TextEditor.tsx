f95f93c34fed0ba74f50bab88dff40c0711d26ff
Indranil Halder <indranilhalder.dev@gmail.com>
2023-02-05 16:02:33 +0530

    Markdown Code Editor

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,10 +1,44 @@
-import { FC } from "react";
+import { FC, useState, useEffect, useRef } from "react";
 import MDEditor from "@uiw/react-md-editor";
+import "./TextEditor.css";
 
 const TextEditor: FC = () => {
+  const editorRef = useRef<HTMLDivElement | null>(null);
+  const [text, setText] = useState<string | undefined>("# Header");
+  const [editing, setEditing] = useState<boolean>(false);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        editorRef.current &&
+        event.target &&
+        editorRef.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+
+    document.addEventListener("click", listener, { capture: true });
+
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div className="text-editor" ref={editorRef}>
+        <MDEditor value={text} onChange={setText} />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <MDEditor />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={text} />
+      </div>
     </div>
   );
 };
